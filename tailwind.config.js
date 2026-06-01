90ef78522770e852b4cbdc492e69c77a3f31cd39
aWFF77 <136655349@qq.com>
2026-06-01 14:40:08 +0800

    添加 darkMode class 支持

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ['./src/renderer/index.html', './src/renderer/src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
